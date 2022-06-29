dcacab3083cb761806f112d9c4c414526e00aa89
Gerardo J. Montivero <gerardo.montivero@gmail.com>
2022-06-29 19:05:52 -0300

    Agregando data

--- src/Controller/ZinicioController.php
+++ src/Controller/ZinicioController.php
@@ -9,7 +9,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class ZinicioController extends AbstractController
 {
     /**
-     * @Route("/zinicio", name="app_zinicio")
+     * @Route("/", name="app_zinicio")
      */
     public function index(): Response
     {
